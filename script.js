0e873620549c2f532b826a7c7dee32d69e194418
OrcaGSM <khisroclash@gmail.com>
2025-08-05 21:07:31 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+.logo-wrapper {
+  display: flex;
+  justify-content: center; /* وسط‌چین افقی */
+  align-items: center;
+  gap: 10px;
+  padding: 20px;
+}
+
+.logo {
+  max-width: 150px;
+  height: auto;
+  object-fit: contain;
+}
